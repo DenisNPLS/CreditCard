446cc9d18d8434d51d5ed6a699a178791d44fd83
DenisNPLS <sjemin1999@mail.ru>
2022-08-18 18:39:51 +0300

    First commit

---
+++ src/test/java/ru/netology/CreditCardTest.java
@@ -0,0 +1,83 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreditCardTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+
+    @Test
+    void SendForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Семин Денис");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79109009593");
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
+
+    @Test
+    void SendFailersName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Semin Denis");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79109009593");
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("input__sub")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    }
+
+    @Test
+    void SendFormWithoutCheckbox() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Семин Денис");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79109009593");
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("checkbox__text")).getText().trim();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text);
+    }
+
+    @Test
+    void SendAnEmptyForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("input__sub")).getText().trim();
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+
+}
